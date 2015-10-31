2af1334fe8ab2f1305037697ae26f66a87e274b8
Jacob <jacob.robert.005@gmail.com>
2015-10-30 22:07:13 -0500

    Final Game w/ Comments

--- Classes.py
+++ Classes.py
@@ -1,12 +1,13 @@
-import pygame
+import pygame                               #Import modules
 import random
+import time
 pygame.init()
 
-SUBTILES = 100
-ALPHA = (255,   0, 255)
+SUBTILES = 100              #Pixels in Tiles
+ALPHA = (255,   0, 255)     #Set Color Preset
 
-class Player(pygame.sprite.Sprite):                             
-    def __init__(self):
+class Player(pygame.sprite.Sprite):         #Create Player                         
+    def __init__(self):                     #Set Attributes and Functions
         super().__init__()
         self.rotation = "Right"  
         self.rotatable = False  
@@ -17,7 +18,7 @@ class Player(pygame.sprite.Sprite):
         self.easy = True        #Rotate to left or right
         self.speed = 4
         self.x = 0
-        self.imageR  = pygame.image.load("robot2.png").convert()
+        self.imageR  = pygame.image.load("robot2.png").convert()        #Preloads all rotated images
         self.imageL  = pygame.transform.flip(self.imageR, True, False)
         self.imageRU = pygame.transform.rotate(self.imageR,  90)
         self.imageLU = pygame.transform.rotate(self.imageL, -90)
@@ -25,22 +26,23 @@ class Player(pygame.sprite.Sprite):
         self.imageLD = pygame.transform.rotate(self.imageR, -90)
         self.Image   = self.imageR
 
-        self.tunnelpos = [2,0]
-        self.cachetunnel =[2,0]
+        self.tunnelpos = [2,0]      #Tilemap position
+        self.cachetunnel =[2,0]     #Previous Tilemap position
         self.inventory = 0
-        self.rect = self.Image.get_rect()
+        self.rect = self.Image.get_rect()#Finds Dimensions from image for collision
+        self.deathtime = None
 
-    def move(self,x,y,diro,obstacle):
+    def move(self,x,y,diro):        #Move Player Function
             
-        if self.queue == 0:
+        if self.queue == 0:         #Move 100 pixels, 1 tile
             self.queue += 24
 
         if self.pos[y] % SUBTILES == 0:
             self.change[x] = self.speed * diro
                   
-    def rotate(self, con1, con2, rota1, rota2, fin1, fin2):
-        if self.rotatable:      #If it's allowed to rotate
-            if self.easy:       #If it's rotating to left or right
+    def rotate(self, con1, con2, rota1, rota2, fin1, fin2):#Rotate Player   Conditions and Rotations and Final
+        if self.rotatable:              #If it's allowed to rotate
+            if self.easy:               #If it's rotating to left or right
                 self.rotation = rota1
             if self.rotation in con1:
                 self.rotation = rota1
@@ -54,79 +56,33 @@ class Player(pygame.sprite.Sprite):
                 
             self.rotatable = False
 
-    def die(self):
-        self.Image = pygame.transform.rotate(self.Image,1)
-        #self.change = [0,0]
+    def die(self):  #Death function
+        if self.deathtime == None:                       #Sets time for death animation       
+            self.deathtime = time.time()
+        elif self.deathtime + 1 > time.time():
+            self.Image = pygame.transform.rotate(self.Image,1)
 
-class Enemy1(pygame.sprite.Sprite):
+class Enemy1(pygame.sprite.Sprite):#Create Enemy
     def __init__(self):
         super().__init__()
-        self.imagel = pygame.image.load("enemy.png").convert()
+        self.imagel = pygame.image.load("enemy.png").convert()      
         self.imager = pygame.transform.flip(self.imagel,True,False)
         self.image = self.imager
         self.image.set_colorkey(ALPHA)
         self.rect  = self.image.get_rect()
-        self.tunne = [3,4]
-        self.diro  = -1
-        self.delay = 0
         self.change = [0,0]
         
-    
-class projectile(pygame.sprite.Sprite):
-    def __init__(self,enemycoord,enemydiro):
-        super().__init__()
-        self.imagera = pygame.image.load("water1.png").convert()
-        self.imagerb = pygame.image.load("water2.png").convert()
-        self.imagerc = pygame.image.load("water3.png").convert()
-        self.imagela = pygame.transform.flip(self.imagera,True,False)
-        self.imagelb = pygame.transform.flip(self.imagerb,True,False)
-        self.imagelc = pygame.transform.flip(self.imagerc,True,False)
         
-        self.image   = self.imagera
-        self.rect    = self.image.get_rect()
-        self.rect.x  = enemycoord[0] + 100 * enemydiro
-        self.rect.y  = enemycoord[1]
-        self.diro    = enemydiro
-    def update(self):
-        delay = 0
-        self.image.set_colorkey(ALPHA)
-        if self.diro == 1:#right
-            self.image = self.imagera
-        if self.diro == -1:#left
-            self.image = self.imagela
-        if delay < 5:
-            delay += 1
-            self.rect.x += 1 * self.diro
-        if delay > 4 and delay < 9:
-            delay += 1
-            self.rect.x += 1 * self.diro
-            if self.image == self.imagera:
-                self.image = self.imagerb
-            if self.image == self.imagela:
-                self.image = self.imagelb
-        if delay > 8 and delay < 13:
-            delay += 1
-            self.rect.x += 1 * self.diro
-            if self.image == self.imagerb:
-                self.image = self.imagerc
-            if self.image == self.imagelb:
-                self.image = self.imagelc
-       # else:
-       #     pygame.sprite.kill
-            
-            
-
-class Objective(pygame.sprite.Sprite):
+class Objective(pygame.sprite.Sprite):#Create Objective
     def __init__(self):
         super().__init__()
         self.image  = pygame.image.load("Crystal.png").convert()
         self.image.set_colorkey(ALPHA)
         self.rect   = self.image.get_rect()
         self.coord  = [[0,400],[400,600],[1000,500]]
-        self.number = 3
-        
+                
 
-class Obstacle(pygame.sprite.Sprite):
+class Obstacle(pygame.sprite.Sprite):#Create Obstacle
     def __init__(self):
         super().__init__()
         self.image  = pygame.image.load("boulder.png").convert()
@@ -135,12 +91,11 @@ class Obstacle(pygame.sprite.Sprite):
                        [0,300],[100,300],[0,500],[500,500],[600,500],[700,500],
                        [900,300],[900,400],[1000,300],[1100,300],[1100,400]]
         self.rect   = self.image.get_rect()
-        self.number = 16
-            
                     
-class Tiles():
+                    
+class Tiles():      #Create Tilemap
     def __init__(self):
-        Alpha = pygame.image.load("Alpha.png").convert()            #Load Images
+        Alpha = pygame.image.load("Alpha.png").convert()            #Preload Images
         Alpha.set_colorkey((255,255,255))
         Up = pygame.image.load("TunnelU.png").convert()
         Do = pygame.image.load("TunnelD.png").convert()
@@ -158,15 +113,15 @@ class Tiles():
         TU = pygame.transform.rotate(TR,  90)
         AL = pygame.image.load("TunnelAll.png").convert()
         
-        self.texture     = {
-                            0b1111: Alpha,  #Up  Down  Left  Right
+        self.texture     = {        #Associate each image with these variables
+                            0b1111: Alpha,
                             0b0111: Up,  
                             0b1011: Do,     
-                            0b1101: Le, 
-                            0b1110: Ri,     
+                            0b1101: Le,         #     Up  Down  Left Right
+                            0b1110: Ri,         # 0b   1     1     1     1  
                             0b0011: UD,
-                            0b1100: LR,
-                            0b0110: UR,
+                            0b1100: LR,         #    0 = Opened
+                            0b0110: UR,         #    1 = Closed
                             0b0101: UL,
                             0b1010: DR,
                             0b1001: DL,
--- Game.py
+++ Game.py
@@ -1,8 +1,9 @@
-import Classes                                    
+import Classes                                              #Imports modules
 import pygame
 import random
+import time
 
-BLACK = (  0,   0,   0)
+BLACK = (  0,   0,   0)                                     #Sets Color Presets
 WHITE = (255, 255, 255)
 BLUE  = (  0,   0, 255)
 ALPHA = (255,   0, 255)
@@ -17,30 +18,29 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Crusader Dig")              #Title
 
-    player  = Classes.Player()  #Sets Class Variable
+    player  = Classes.Player()                              #Creates Instance of Player and Tunnel System
     tunnels = Classes.Tiles()
         
-    background = pygame.image.load("Level.png").convert()
-    explosion  = pygame.image.load("Explosion.png").convert()
+    background = pygame.image.load("Level.png").convert()   #Uploads Images
+    explosion  = pygame.image.load("explosion.png").convert()
     explosion.set_colorkey(ALPHA)
     
-    gameover = pygame.image.load("game over.png").convert()
-    gameover.set_alpha(1)
-    youwin   = pygame.image.load("you win.png").convert()
-    youwin.set_alpha(1)
+    gameover = pygame.image.load("game over.png").convert_alpha()
+    youwin   = pygame.image.load("you win.png").convert_alpha()
+    
 
     done = False                                            #Set While Variable
     clock = pygame.time.Clock()
     
-    obdiro = 0b1111
+    obdiro = 0b1111                                         #Preset info to add tunnels
     indiro = 0b1111
 
-    objlist = pygame.sprite.Group()
+    objlist = pygame.sprite.Group()                         #Creates lists of entities
     obslist = pygame.sprite.Group()
     enemylist = pygame.sprite.Group()
     deathlist = pygame.sprite.Group()
 
-    for obj in range(3):
+    for obj in range(3):                                    #Creates Entities and Adds them to Lists
         objective = Classes.Objective()
         objective.rect.x = objective.coord[obj][0]
         objective.rect.y = objective.coord[obj][1]
@@ -65,26 +65,26 @@ def main():
         """ Main Game Loop """
     
         for event in pygame.event.get():            
-            if event.type == pygame.QUIT:           #X Exits
-                main()
+            if event.type == pygame.QUIT:           #Restart Game
+                done = True
                 
-            if event.type == pygame.KEYDOWN:        #Arrows Move
+            if event.type == pygame.KEYDOWN:        #Restart Game
                 if event.key == pygame.K_ESCAPE:
-                    main()
+                    done = True
                 
-                if event.key == pygame.K_LEFT:      
+                if event.key == pygame.K_LEFT:
                     player.easy = True
                     if player.change == [0,0]:
-                        player.move(0,1,-1,obstacle)
+                        player.move(0,1,-1)#Moves Player
                     player.rotatable = True
-                    obdiro = 0b1110
+                    obdiro = 0b1110                 #Tunnel Directions
                     indiro = 0b1101
-                    player.rotate(FILL,FILL, "Left",FILL, player.imageL, FILL)
+                    player.rotate(FILL,FILL, "Left",FILL, player.imageL, FILL)#Rotates PLayer
 
                 if event.key == pygame.K_RIGHT:
                     player.easy = True
                     if player.change == [0,0]:
-                        player.move(0,1, 1,obstacle)
+                        player.move(0,1, 1)
                     player.rotatable = True
                     obdiro = 0b1101
                     indiro = 0b1110
@@ -93,7 +93,7 @@ def main():
                 if event.key == pygame.K_UP:
                     player.easy = False
                     if player.change == [0,0]:
-                        player.move(1,0,-1,obstacle)
+                        player.move(1,0,-1)
                     player.rotatable = True
                     backgroundcoord = 1
                     obdiro = 0b1011
@@ -103,27 +103,22 @@ def main():
                 if event.key == pygame.K_DOWN:
                     player.easy = False
                     if player.change == [0,0]:
-                        player.move(1,0, 1,obstacle)
+                        player.move(1,0, 1)
                     player.rotatable = True
                     obdiro = 0b0111
                     indiro = 0b1011
                     player.rotate(["Right","LeftUp"],["Left","RightUp"],"LeftDown","RightDown", player.imageLD, player.imageRD)
+                    
 
-        if player.change[0] > 0 or player.change[0] < 0:
-            player.change[1] 
+        if player.change[0] > 0 or player.change[0] < 0:    #Allows Player to move on only one axis at a time    
+            player.change[1] = 0
         if player.change[1] > 0 or player.change[1] < 0:
-            player.change[0] 
+            player.change[0] = 0
         
-        for obstacle in obslist:
-            if player.rect.colliderect(obstacle.rect):
-                player.pos[0] -= player.change[0]
-                player.pos[1] -= player.change[1]
-
-                player.change[:] = [0, 0]
-
-        player.pos[0] += player.change[0]
+        player.pos[0] += player.change[0]               #Move Player
         player.pos[1] += player.change[1]
-        if player.pos[1] < 200:
+        
+        if player.pos[1] < 200:                         #Set limits at edges of screen
             player.pos[1] = 200
         if player.pos[1] > 600:
             player.pos[1] = 600
@@ -136,10 +131,11 @@ def main():
         else:
             player.change = [0,0]
             
-        player.rect.x = player.pos[0]
+        player.rect.x = player.pos[0]                   #Sets Collision Box
         player.rect.y = player.pos[1]
 
-        if player.change == [0,0]:
+
+        if player.change == [0,0]:                                                   #Adds tunnels
             player.tunnelpos[0] = player.pos[0]//100
             player.tunnelpos[1] = player.pos[1]//100 -2
             if not player.tunnelpos[1] == 0:
@@ -149,28 +145,32 @@ def main():
                 tunnels.addtunnel(player.cachetunnel[0],player.cachetunnel[1],indiro)
             player.cachetunnel = player.tunnelpos
 
-        if player.change == [0,0]:
+        for obstacle in obslist:                        #Checks if Player collides with obstacle
+            if player.rect.colliderect(obstacle.rect):
+                player.pos[0] -= player.change[0]       #Bounces Back Player
+                player.pos[1] -= player.change[1]
+
+                player.change[:] = [0, 0]
+
+
+        if player.change == [0,0]:                                          #Delete and record collisions with objectives
             objcollide = pygame.sprite.spritecollide(player,objlist,True)    
-
         for objective in objcollide:
-            player.inventory +=1
+            player.inventory +=1    
 
-                        
-        backgroundcoord = [0,0]
-        screen.blit(background,backgroundcoord)    
 
-        for enemy in enemylist:
+        for enemy in enemylist:                                                    #Access each enemy
             if enemy.change == [0, 0]:
-                enemy.change[:] = random.choice([(-1, 0), (1, 0), (0, 1), (0, -1)])
+                enemy.change[:] = random.choice([(-1, 0), (1, 0), (0, 1), (0, -1)])#Randomly choose direction
             if (enemy.rect.y + enemy.rect.x) % 100 == 0:
                 x = enemy.rect.x // 100
                 y = enemy.rect.y // 100 - 2
                 tile = tunnels.tilemap[y][x]
                 if random.random() > 0.66:
-                    enemy.change[:] = [0, 0]
+                    enemy.change[:] = [0, 0]                    
                 elif y == 0:
                     enemy.change[:] = [0, 1]
-                elif tile & 0b1000 and enemy.change[1] == -1:
+                elif tile & 0b1000 and enemy.change[1] == -1:           #Check for tunnel
                     enemy.change[1] = 0
                 elif tile & 0b0100 and enemy.change[1] == 1:
                     enemy.change[1] = 0
@@ -180,55 +180,57 @@ def main():
                     enemy.change[0] = 0
  
             x, y = enemy.change
-            if enemy.rect.y + y >= 600 or enemy.rect.y + y <= 0 or enemy.rect.x + x >= 1200 or enemy.rect.x + x <= 0:
+            if enemy.rect.y + y >= 600 or enemy.rect.y + y <= 0 or enemy.rect.x + x >= 1200 or enemy.rect.x + x <= 0: #Enemy can't leave screen
                 enemy.change[:] = [0, 0]
             else:
-                enemy.rect.y += y * 2
+                enemy.rect.y += y * 2               #Move Enemy
                 enemy.rect.x += x * 2
                 
-            if enemy.change[0] == 1:
+            if enemy.change[0] == 1:                #Rotate Enemy
                 enemy.image = enemy.imager
             if enemy.change[0] == -1:
                 enemy.image = enemy.imagel
             enemy.image.set_colorkey(ALPHA)
 
-       
-        for row in range(13):
+
+
+
+        backgroundcoord = [0,0]                         #Draw the Background
+        screen.blit(background,backgroundcoord)
+
+        
+        for row in range(13):                           #Draw Tunnels
             for column in range(12):
                 screen.blit(tunnels.texture[tunnels.tilemap[row][column]],(column*100,row*100+200))
 
-        objlist.draw(screen)
-        obslist.draw(screen)
-        enemylist.draw(screen)
-        
+        objlist.draw(screen)                            #Draw Objectives
+        obslist.draw(screen)                            #Draw Obstacles
+        enemylist.draw(screen)                          #Draw Enemies
 
         player.Image.set_colorkey(ALPHA)
-        screen.blit(player.Image,player.pos)        #Draw Player
+        screen.blit(player.Image,player.pos)            #Draw Player
 
-        deathcollide = pygame.sprite.spritecollide(player,deathlist,False)
+        deathcollide = pygame.sprite.spritecollide(player,deathlist,False) #Check for collisions between Player and Enemies
         for death in deathcollide:
-            player.life = 0
             deathtime = 30
-            if deathtime > 0:
+            if deathtime > 0:                       #Draw Death Animation
                 player.die()
                 screen.blit(explosion,player.pos)
-                deathtime -= 1 
-            else:
-                done = True
-
-        if player.life == 0:        
-            screen.blit(gameover, [0,0])
-        if player.inventory == 3:
-            screen.blit(youwin, [0,0])
-
+                deathtime -=1
+            player.life = 0
         
-        pygame.display.flip()
-        clock.tick(60)                                      #Set Framerate
+        if player.life == 0:                #If dead
+            screen.blit(gameover, [0,0])      #Draw Game Over
+        if player.inventory == 3:           #If all objectives obtained
+            screen.blit(youwin, [0,0])        #Draw You Win!
 
-    for reset in pygame.event.get():
-        main()
 
-if __name__ == "__main__":
+        pygame.display.flip()               #Update Screen
+        clock.tick(60)                      #Set Framerate
+
+    main()                                  #run game
+
+if __name__ == "__main__":                  #Run game when opened
     main()
 
 
